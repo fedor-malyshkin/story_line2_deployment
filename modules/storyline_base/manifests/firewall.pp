5248b7c5e3ed5099555db2aa171cf614a9558975
fedor <fedor.malyshkin@yandex.ru>
2017-03-18 23:02:52 +0300

    +

--- modules/storyline_base/manifests/firewall.pp
+++ modules/storyline_base/manifests/firewall.pp
@@ -1,7 +1,7 @@
 class storyline_base::firewall {
 
 
-class { ['storyline_base::firewall_pre', 'storyline_base::firewall_post']: }
+# class { ['storyline_base::firewall_pre', 'storyline_base::firewall_post']: }
 
 Firewall {
   before  => Class['storyline_base::firewall_post'],

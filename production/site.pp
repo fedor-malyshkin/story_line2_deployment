b4b5e1aa9f90ea67f6511add8fea061ef07c14d2
fedor <fedor.malyshkin@yandex.ru>
2017-03-27 19:40:54 +0300

    move elasticsearch to ci.nlp-project.ru

--- production/site.pp
+++ production/site.pp
@@ -3,13 +3,15 @@ node "datahouse01.nlp-project.ru"  {
 		include storyline_base::ntp
 		include storyline_base::srv_oper
 		include storyline_base::ssh
-		include storyline_infra::collectd
 		include storyline_base::oracle_java
-		include storyline_infra::elasticsearch
+
+		include storyline_infra::collectd
+
 		include storyline_infra::zookeeper
 		include storyline_infra::mongodb
 
 		include storyline_components::crawler
+
 		include storyline_components::server_storm
 
 		include storyline_components::server_web
@@ -17,4 +19,21 @@ node "datahouse01.nlp-project.ru"  {
 
 		include storyline_infra::monit
 		include storyline_base::firewall
+}
+
+node "ci.nlp-project.ru"  {
+		include storyline_base::ntp
+		include storyline_base::srv_oper
+		include storyline_base::ssh
+		include storyline_base::oracle_java
+
+		include storyline_infra::collectd
+		
+		include storyline_infra::influxdb
+		include storyline_infra::elsticsearch
+		include storyline_infra::grafana
+
+		include storyline_infra::monit
+		include storyline_base::firewall
+
 }
